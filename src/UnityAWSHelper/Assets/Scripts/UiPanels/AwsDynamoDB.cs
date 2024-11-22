40b2f9283f25c58e534a04ccdf47b39507bac2ae
XeldarAlz <xeldaralz@hotmail.com>
2024-11-22 11:19:28 +0300

    Add download data from DynamoDB

--- src/UnityAWSHelper/Assets/Scripts/UiPanels/AwsDynamoDB.cs
+++ src/UnityAWSHelper/Assets/Scripts/UiPanels/AwsDynamoDB.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
+using Amazon.CognitoIdentityProvider.Model;
 using Amazon.DynamoDBv2.Model;
 using Managers;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace UiPanels
 {
@@ -21,27 +24,84 @@ namespace UiPanels
                 AwsUiManager.Instance.SetFeedbackText(e.Message);
             }
         }
+
+        public async void DownloadItem()
+        {
+            try
+            {
+                bool success = await DownloadItemAsync();
+                // AwsUiManager.Instance.SetFeedbackText($"DynamoDB download status: {success}");
+            }
+            catch (Exception e)
+            {
+                AwsUiManager.Instance.SetFeedbackText(e.Message);
+            }
+        }
+        
+        private async Task<bool> DownloadItemAsync()
+        {
+            try
+            {
+                var currentUserResponse = await AwsSdkManager.Instance.GetCurrentUserResponse();
+                var email = currentUserResponse.UserAttributes.Find(attribute => attribute.Name.Equals("email")).Value;
+                var result = await FetchUserData(email);
+
+                if (result != null)
+                {
+                    string feedback = $"Email: {result["email"].S}, Score: {result["score"].N}";
+                    AwsUiManager.Instance.SetFeedbackText(feedback);
+                    return true;
+                }
+                
+                return false;
+            }
+            catch (Exception e)
+            {
+                AwsUiManager.Instance.SetFeedbackText($"{e.Message}");
+                return false;
+            }
+        }
+        
+        private async Task<Dictionary<string, AttributeValue>> FetchUserData(string email)
+        {
+            var client = AwsSdkManager.Instance.GetDynamoDBClient();
+
+            var request = new GetItemRequest
+            {
+                TableName = "MyTable",
+                Key = new Dictionary<string, AttributeValue>
+                {
+                    ["email"] = new AttributeValue { S = email }
+                }
+            };
+
+            var response = await client.GetItemAsync(request);
+            return response.HttpStatusCode == HttpStatusCode.OK ? response.Item : null;
+        }
+
         
         private static async Task<bool> UploadItemAsync()
         {
             var client = AwsSdkManager.Instance.GetDynamoDBClient();
+            var currentUserResponse = await AwsSdkManager.Instance.GetCurrentUserResponse();
+            var email = currentUserResponse.UserAttributes.Find(attribute => attribute.Name.Equals("email")).Value;
             
             // S = String
             // N = Number
             // L = List
             var item = new Dictionary<string, AttributeValue>
             {
-                ["email"] = new AttributeValue { S = "testingTwo@gmail.com" },
-                ["score"] = new AttributeValue { N = "500" },
-                // ["itemsFound"] = new AttributeValue()
-                // {
-                //     L =
-                //     {
-                //         new AttributeValue { N = "1"},
-                //         new AttributeValue { N = "12"},
-                //         new AttributeValue { N = "85"}
-                //     }
-                // }
+                ["email"] = new() { S = $"{email}" },
+                ["score"] = new() { N = $"{Random.Range(0, 500)}" },
+                ["itemsFound"] = new()
+                {
+                    L =
+                    {
+                        new AttributeValue { N = $"{Random.Range(0, 10)}"},
+                        new AttributeValue { N = $"{Random.Range(0, 100)}"},
+                        new AttributeValue { N = $"{Random.Range(0, 1000)}"}
+                    }
+                }
             };
 
             // DynamoDB Table name
